139b715fa5fb05628834998a34ceba3f25fd61e9
Anahi Rendon <89081401+arendone@users.noreply.github.com>
2022-06-04 17:02:49 +0200

    add exit

--- exit/error.c
+++ exit/error.c
@@ -1,1 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpfuhl <jpfuhl@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/03 14:15:56 by arendon-          #+#    #+#             */
+/*   Updated: 2022/05/23 14:54:49 by jpfuhl           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "../header/exit.h"
+
+void	error_exit(char *arg, char *msg)
+{
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
+	ft_putstr_fd(arg, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd(msg, STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
+}
+
+void	error_exit_specified(char *arg1, char *arg2, char *msg, int code)
+{
+	ft_putstr_fd(arg1, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd(arg2, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd(msg, STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
+	g_exit_status = code;
+}
+
+void	error_exit_three(char *arg1, char *arg2, char *msg, int code)
+{
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
+	ft_putstr_fd(arg1, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd(arg2, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd(msg, STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
+	g_exit_status = code;
+}
+
+void	error_unexpectedtoken(char *token, int code)
+{
+	ft_putstr_fd("minishell: syntax error near unexpected token `",
+		STDERR_FILENO);
+	ft_putstr_fd(token, STDERR_FILENO);
+	ft_putstr_fd("'\n", STDERR_FILENO);
+	g_exit_status = code;
+}
+
+int	exit_set_error(char *path, char *cmd)
+{
+	if (errno == ENOENT)
+	{
+		error_exit(path, "No such file or directory");
+		exit(127);
+	}
+	else if (errno == EACCES)
+	{
+		error_exit(cmd, "Permission denied");
+		exit(1);
+	}
+	else
+	{
+		error_exit(cmd, "command not found");
+		exit(127);
+	}
+}
